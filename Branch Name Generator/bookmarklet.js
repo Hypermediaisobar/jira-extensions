b03241c91847457e6fec2e17bd02dea6dc6bb191
Michał Kurowski <michalkurowskix@gmail.com>
2017-01-17 16:25:23 +0100

    chrome boorkmarklet enriching jira & github workflow

---
+++ Branch Name Generator/bookmarklet.js
@@ -0,0 +1,26 @@
+javascript:(function () {
+	var cursorX = void 0;
+	var cursorY = void 0;
+
+	sasasfunction onMouseMove(evt) {
+		cursorX = evt.clientX;
+		cursorY = evt.clientY;
+	}
+
+	function onKeyPress(evt) {
+		var element = document.elementFromPoint(cursorX, cursorY);
+		if (evt.charCode === 103) {
+			var ticketTitle = $(element).closest(".ghx-issue")[0].getAttribute("aria-label");
+			var gitFriendlyTicketName = ticketTitle.replace(/\s-\s|\s|\//g, "_");
+			var resultsContainer = document.createElement("input");
+			resultsContainer.value = "\"#" + gitFriendlyTicketName + "\"";
+			document.body.appendChild(resultsContainer);
+			resultsContainer.select();
+			document.execCommand("selectAll");
+			document.execCommand("copy", false, null);
+			document.body.removeChild(resultsContainer);
+		}
+	}
+	document.querySelector(".ghx-swimlane").addEventListener("mousemove", _.throttle(onMouseMove, 300));
+	document.addEventListener("keypress", onKeyPress);
+})()
